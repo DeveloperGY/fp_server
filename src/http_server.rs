666d311293bc85ebcc2648602bf345a07703f958
DeveloperGY <dev.garrettyenojr@gmail.com>
2023-11-01 00:13:21 -0400

    Added interface for server state

--- src/http_server.rs
+++ src/http_server.rs
@@ -8,6 +8,7 @@ use std::net::{
 };
 use std::collections::HashMap;
 use std::io::{Read, Write};
+use std::sync::{Arc, Mutex};
 
 pub use request::{Request, RequestMethod};
 pub use response:: *;
@@ -17,16 +18,21 @@ use request::RequestParser;
  * An HTTP Server
  * Currently only works for HTTP 1.1
  */
-pub struct HTTPServer {
+pub struct HTTPServer<T> 
+    where T: Sync + Send
+{
     listener: TcpListener,
-    handlers: HashMap<RequestMethod, Option<fn(Request) -> Response>>
+    handlers: HashMap<RequestMethod, Option<fn(Request, Arc<Mutex<T>>) -> Response>>,
+    server_state: Arc<Mutex<T>>
 }
 
-impl HTTPServer {
+impl<T> HTTPServer<T> 
+    where T: Sync + Send
+{
     /**
      * Creates an HTTP Server
      */
-    pub fn new(addr: impl ToSocketAddrs) -> Result<Self, String> {
+    pub fn new(addr: impl ToSocketAddrs, server_state: Arc<Mutex<T>>) -> Result<Self, String> {
         let listener = match TcpListener::bind(addr) {
             Ok(listener) => listener,
             Err(_) => return Err("Error: Failed to bind to address!".into())
@@ -46,7 +52,8 @@ impl HTTPServer {
         
         let server = Self {
             listener,
-            handlers
+            handlers,
+            server_state: server_state.clone()
         };
 
         Ok(server)
@@ -55,7 +62,7 @@ impl HTTPServer {
     /**
      * Sets the handler for the given HTTP Request method
      */
-    pub fn handle(&mut self, method: RequestMethod, handler: Option<fn(Request) -> Response>) {
+    pub fn handle(&mut self, method: RequestMethod, handler: Option<fn(Request, Arc<Mutex<T>>) -> Response>) {
         self.handlers.insert(method, handler);
     }
 
@@ -66,6 +73,7 @@ impl HTTPServer {
         std::thread::scope(|s| {
             for connection in self.listener.incoming() {
                 if let Ok(mut stream) = connection {
+                    let state = self.server_state.clone();
                     s.spawn(move || {
                         let request_bytes = match self.receive(&mut stream) {
                             Ok(bytes) => bytes,
@@ -92,7 +100,7 @@ impl HTTPServer {
                         }
         
                         if let Some(Some(handler)) = self.handlers.get(&request.method) {
-                            let response = handler(request);
+                            let response = handler(request, state);
                             stream.write_all(&response.parse()).unwrap();
                         }
                         else {
